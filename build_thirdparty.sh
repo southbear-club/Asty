9cf6b5f2a181f9d9547319c2a5395976e653545e
wotsen <astralrovers@outlook.com>
2021-01-10 14:56:25 +0800

    修改依赖

--- build_thirdparty.sh
+++ build_thirdparty.sh
@@ -3,32 +3,24 @@
 TOP_DIR=`pwd`
 
 cd thirdparty
-wget https://github.com/apache/thrift/archive/v0.13.0.tar.gz
-tar -xf v0.13.0.tar.gz
-rm v0.13.0.tar.gz
-cd thrift-0.13.0
-./bootstrap.sh
-./configure --prefix=${TOP_DIR} --with-ruby=no --with-java=no --with-perl=no \
+
+wget https://github.com/libevent/libevent/releases/download/release-2.1.12-stable/libevent-2.1.12-stable.tar.gz && tar -xf libevent-2.1.12-stable.tar.gz && rm libevent-2.1.12-stable.tar.gz
+cd libevent-2.1.12-stable && ./configure --prefix=/usr --enable-static --enable-shared
+make -j4 && sudo make install
+cd .. && rm -rf libevent-2.1.12-stable
+
+wget https://github.com/apache/thrift/archive/v0.13.0.tar.gz && tar -xf v0.13.0.tar.gz && rm v0.13.0.tar.gz
+cd thrift-0.13.0 && ./bootstrap.sh && ./configure --prefix=/usr --with-ruby=no --with-java=no --with-perl=no \
     --with-php=no --with-csharp=no --with-erlang=no --with-lua=no --with-qt5=no \
     --with-php_extension=no --with-dart=no --with-haskell=no --with-cl=no --with-haxe=no --with-netstd=no --with-d=no \
     --with-rs=no --with-python=no --with-nodejs=no
-make CPPFLAGS=-DFORCE_BOOST_SMART_PTR -j 4 -s
-make install
-cd ..
-rm -rf thrift-0.13.0
+make CPPFLAGS=-DFORCE_BOOST_SMART_PTR -j4 -s && sudo make install
+cd .. && rm -rf thrift-0.13.0
 
-wget https://github.com/google/glog/archive/v0.4.0.tar.gz
-tar -xf v0.4.0.tar.gz
-rm v0.13.0.tar.gz
-cd glog-0.4.0
-cmake -H. -Bbuild -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=${TOP_DIR}
-cmake --build build
-cmake --build build --target install
-rm -rf glog-0.4.0
+wget https://github.com/google/glog/archive/v0.4.0.tar.gz && tar -xf v0.4.0.tar.gz && rm v0.4.0.tar.gz
+cd glog-0.4.0 && cmake -H. -Bbuild -G "Unix Makefiles" && cmake --build build && sudo cmake --build build --target install
+cd .. && rm -rf glog-0.4.0
 
-cd googletest
-cmake -H. -Bbuild -DCMAKE_INSTALL_PREFIX=${TOP_DIR}
-cmake --build build
-cmake --build build --target install
+sudo apt-get install -y cmake libgtest-dev && cd /usr/src/googletest && sudo cmake . && sudo make && sudo make install && sudo rm /usr/src/googletest -rf && cd -
 
 cd ..
